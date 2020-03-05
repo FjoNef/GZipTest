1d1d8d93a5068c127d86fb0566113ac7bb227873
Фёдор Нефедьев <fjodor95@hotmail.com>
2020-03-05 04:46:20 +0300

    Final cleanup

--- GZipTest/GZipper.cs
+++ GZipTest/GZipper.cs
@@ -8,28 +8,6 @@ namespace GZipTest
         public static readonly Int32 BUFF_SIZE = 1024 * 1024;
         public static readonly Int32 PROC_COUNT = Environment.ProcessorCount;
 
-        private static readonly byte[] _headerBytes = new byte[]
-        {
-            0x1F, // ID1
-            0x8B, // ID2
-            0x8,  // CM = deflate
-            0,    // FLG, no text, no crc, no extra, no name, no comment
-
-            // MTIME (Modification Time) - no time available
-            0,
-            0,
-            0,
-            0,
-
-            // XFL
-            // 2 = compressor used max compression, slowest algorithm
-            // 4 = compressor used fastest algorithm
-            4,
-
-            // OS: 0 = FAT filesystem (MS-DOS, OS/2, NT/Win32)
-            0
-        };
-
         public static void Compress(String inputFileName, String outputFileName = null)
         {
             FileInfo inputFile = new FileInfo(inputFileName);
@@ -45,12 +23,6 @@ namespace GZipTest
 
             if (compressor.InnerException != null)
                 throw compressor.InnerException;
-        }
-
-        private static void CompressEmptyFile()
-        {
-            //streamSyncronizer.WriteToFile(_headerBytes, 0, _headerBytes.Length);
-            //streamSyncronizer.WriteToFile(new byte[8], 0, 8);
         }
 
         public static void Decompress(String inputFileName, String outputFileName = null)
